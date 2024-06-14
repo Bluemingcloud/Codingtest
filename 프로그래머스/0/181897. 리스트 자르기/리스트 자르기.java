ed9062d60e8b4d555cbc9308c1defc38c57fb082
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-14 20:17:54 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,33 +1,14 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-    	int a = slicer[0];
-    	int b = slicer[1];
-    	int c = slicer[2];
-    	int length = 0;
-    	int start = 0;
-    	int end = num_list.length - 1;
-    	int plus = 0;
+
+    	int start = n == 1 ? 0 : slicer[0];
+    	int end = n == 2 ? num_list.length - 1 : slicer[1];
+    	int plus = n == 4 ? slicer[2] : 1;
     	
-        if(n == 1) {
-        	end = b;        	
-        } else if(n == 2) {
-        	start = a;     	
-        } else if(n == 3) {
-        	start = a;
-        	end = b;        	
-        } else {
-        	start = a;
-        	end = b;
-        	plus = c - 1;
-        }
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i = start; i <= end; i += 1 + plus) {
-        	list.add(num_list[i]);
-        }
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < answer.length; i++) {
-        	answer[i] = list.get(i);
+        int[] answer = new int[(end - start) / plus + 1];
+        for(int i = start, j = 0; i <= end; i += plus) {
+        	answer[j++] = num_list[i];
         }       
         
     	return answer;
