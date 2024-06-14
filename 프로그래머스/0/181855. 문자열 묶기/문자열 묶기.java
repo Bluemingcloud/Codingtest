5513b9e0483d363a7fb0e30629f5a967ebb4fe94
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-14 17:50:53 +0900

    [level 0] Title: 문자열 묶기, Time: 5.94 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,7 +1,6 @@
 import java.util.*;
 class Solution {
 	    public int solution(String[] strArr) {
-	        int answer = 0;
 	        int[] count = new int[31];
 	        for(String str : strArr) {
 	            count[str.length()]++;
