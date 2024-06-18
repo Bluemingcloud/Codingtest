e40323fdc1f2d1005398ef1787ff8f8281d61ee7
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-18 21:19:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,18 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    long a = Long.parseLong(st.nextToken());
+    long b = Long.parseLong(st.nextToken());
+    long c = Long.parseLong(st.nextToken());
 
-	public static void main(String[] args) {
-		
-		Scanner scan = new Scanner(System.in);
-		
-		//1 ≤ A, B, C ≤ 10^12 인 값 -> long 을 써야한다.
-		long A = scan.nextLong();
-		long B = scan.nextLong();
-		long C = scan.nextLong();
-		
-		System.out.println(A + B + C);
-		
-		scan.close();
-	}
+    System.out.println(a + b + c);
+  }
 }
