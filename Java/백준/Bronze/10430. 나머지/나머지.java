19c688abe377a49e9cd1e03052f5e28cf76f6025
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-18 21:06:06 +0900

    [Bronze V] Title: 나머지, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- Java/백준/Bronze/10430. 나머지/나머지.java
+++ Java/백준/Bronze/10430. 나머지/나머지.java
@@ -1,21 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    br.close();
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+    int c = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int A = scan.nextInt();
-		int B = scan.nextInt();
-		int C = scan.nextInt();
-		
-		System.out.println((A+B)%C);
-		System.out.println(((A%C)+(B%C))%C);
-		System.out.println((A*B)%C);
-		System.out.println(((A%C)*(B%C))%C);		
-	
-		scan.close();
+    int result1 = (a + b) % c;
+    int result2 = ((a % c) + (b % c)) % c;
+    int result3 = (a * b) % c;
+    int result4 = ((a % c) * (b % c)) % c;
+    
+    StringBuilder sb = new StringBuilder();
+    sb.append(result1).append("\n");
+    sb.append(result2).append("\n");
+    sb.append(result3).append("\n");
+    sb.append(result4);
 
-	}
-
+    System.out.println(sb.toString());
+  }
 }
