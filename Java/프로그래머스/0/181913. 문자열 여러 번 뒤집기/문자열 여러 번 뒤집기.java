261c1be8452c0fdc9f801f99671621fb4769050f
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-19 21:15:50 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 3.47 ms, Memory: 80.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
+++ Java/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.java
@@ -1,10 +1,11 @@
 class Solution {
   public String solution(String my_string, int[][] queries) {
     StringBuilder answer = new StringBuilder(my_string);
-
+    StringBuilder sub = new StringBuilder();
     for(int[] q : queries) {
-      StringBuilder sub = new StringBuilder(answer.substring(q[0], q[1] + 1));
+      sub.append(answer.substring(q[0], q[1] + 1));
       answer.replace(q[0], q[1] + 1, sub.reverse().toString());
+      sub.setLength(0);
     }
     return answer.toString();
   }
