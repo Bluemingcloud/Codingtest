aa34a464fcc439d9a9e50a07db3bad6dc1fe9b38
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-20 18:13:37 +0900

    [level 0] Title: 그림 확대, Time: 0.10 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ Java/프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,26 +1,13 @@
 class Solution {
   public String[] solution(String[] picture, int k) {
     StringBuilder sb = new StringBuilder();
-    StringBuilder sbX = new StringBuilder();
-    StringBuilder sbDot = new StringBuilder();
-    StringBuilder temp = new StringBuilder();
-    for(int i = 0; i < k; i++) {
-      sbX.append("x");
-      sbDot.append(".");
-    }
-
     for(String p : picture) {
-      temp.setLength(0);
-      for(int i = 0; i < p.length(); i++) {
-        if(p.charAt(i) == 'x') {
-          temp.append(sbX.toString());
-        } else {
-          temp.append(sbDot.toString());
-        }
+     for(int i = 0; i < k; i++) {
+      for(int j = 0; j < p.length(); j++) {
+        sb.append(String.valueOf(p.charAt(j)).repeat(k));
       }
-      for(int i = 0; i < k; i++) {
-        sb.append(temp.toString()).append(" ");        
-      }
+      sb.append(" ");
+     }
     }
 
     return sb.toString().split(" ");
