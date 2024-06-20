753353da6ae97fec29997b0dfd193eec35db7aa0
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-20 18:54:07 +0900

    [level 0] Title: 외계어 사전, Time: 0.04 ms, Memory: 68.9 MB -BaekjoonHub

--- Java/프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ Java/프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,31 +1,13 @@
 class Solution {
   public int solution(String[] spell, String[] dic) {
-    int[] count = new int[26];
-
-    for(String s : spell) {
-      count[s.charAt(0) - 'a']++;
-    }
-
-    StringBuilder sb = new StringBuilder();
+    int count;
     for(String d : dic) {
-      sb.setLength(0);
-      for(int i = 0; i < d.length(); i++) {
-        if(count[d.charAt(i) - 'a'] == 1) {
-          sb.append(d.charAt(i));
-        }
+      count = 0;
+      for(String s : spell) {
+        if(d.contains(s)) count++;
       }
-      if(sb.length() == spell.length) {
-        boolean flag = false;
-        for(String s : spell) {
-          if(sb.indexOf(s) != sb.lastIndexOf(s)) {
-            flag = true;
-            break;
-          }
-        }
-        if(!flag) return 1;
-      }
+      if(count == spell.length) return 1;
     }
-
     return 2;
-  }  
+  }
 }
