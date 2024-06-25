38612d6310defab5398e5897dc00f3910784b3a1
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-25 15:09:23 +0900

    [level 0] Title: 로그인 성공?, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- Java/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ Java/프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,25 +1,20 @@
 class Solution {
     public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
-        boolean flag = false;
+
         for(String[] arr : db) {
             
             if(id_pw[0].equals(arr[0])) {
-                flag = false;
+
                 if(id_pw[1].equals(arr[1])) {
                     return "login";
-                } else {
-                    flag = true;
                 }
-                
+                return "wrong pw";
             }
             
         }
-        if(flag) {
-            return "wrong pw";
-        } else {
-            return "fail";    
-        }
+
+        return "fail";    
+
         
         
     }
