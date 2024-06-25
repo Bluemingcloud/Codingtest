82a3f18f4a86437f58696e574c6e4a2928eff9e3
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-25 19:53:29 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- Java/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ Java/프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,14 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
         
-        for(int i = 2; i <= Math.min(a,  b); i++) {
-        	if(a % i == 0 && b % i == 0) {
-        		while(a % i == 0 && b % i == 0) {
-        			a /= i;
-        			b /= i;
-        		}
-        	}
-        }
+    	b /= gcd(a, b);
         
         while(b % 2 == 0) {
         	b /= 2;
@@ -22,7 +15,12 @@ class Solution {
         
         return 2;
         
-        
     }
+    
+    private static int gcd(int a, int b) {
+    	if(b % a == 0) return a;
+    	return gcd(b % a, a);
+    }
+
 
 }
