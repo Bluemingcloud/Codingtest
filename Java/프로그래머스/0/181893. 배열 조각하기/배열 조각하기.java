900666cb407f18ecbd11259a264b88574b1231f2
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-06-26 19:39:27 +0900

    [level 0] Title: 배열 조각하기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ Java/프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -2,26 +2,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int[] query) {
     	
-        Deque<Integer> queue = new LinkedList<Integer>();
-        for(int a : arr) {
-        	queue.offer(a);
-        }
+        int start = 0;
+        int end = arr.length;
         
         for(int i = 0; i < query.length; i++) {
-            int size = queue.size();
-        	if(i % 2 == 0) {
-        		while(queue.size() > query[i] + 1) {
-        			queue.pollLast();
-        		}
-        	} else {
-        		while(queue.size() > size - query[i]) {
-        			queue.pollFirst();
-        		}
-        	}
+        	if(i % 2 == 0) end = start + query[i] + 1;
+        	else start += query[i];
         }
         
-    	
-    	return queue.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.copyOfRange(arr, start, end);
 
     }
 }
