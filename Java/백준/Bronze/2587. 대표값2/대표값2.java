63eb8b1a65dc1f248aa28116ca4b0c0d47747fab
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-07-02 23:54:58 +0900

    [Bronze II] Title: 대표값2, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

--- Java/백준/Bronze/2587. 대표값2/대표값2.java
+++ Java/백준/Bronze/2587. 대표값2/대표값2.java
@@ -13,7 +13,19 @@ public class Main {
       sum += arr[i];
     }
     br.close();
-    Arrays.sort(arr);
+    
+    for(int i = 1; i < arr.length; i++) {
+        int target = arr[i];
+        int j = i - 1;
+        while(j >= 0 && target < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = target;
+    }
+      
+      
+     
     System.out.println(sum / 5);
     System.out.println(arr[2]);
   }
