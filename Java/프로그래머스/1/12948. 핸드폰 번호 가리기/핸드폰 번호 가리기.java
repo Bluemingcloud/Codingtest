05ccb7a17c147ca049e47319525b30aa206a2de2
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-02 20:28:00 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

--- Java/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ Java/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,12 +1,9 @@
 class Solution {
     public String solution(String phone_number) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < phone_number.length(); i++) {
-            sb.append("*");
+        char[] arr = phone_number.toCharArray();
+        for(int i = 0; i < phone_number.length() - 4; i++) {
+            arr[i] = '*';
         }
-        String subNum = phone_number.substring(phone_number.length() - 4);
-        
-        
-        return sb.replace(phone_number.length() - 4, phone_number.length(), subNum).toString();
+        return String.valueOf(arr);
     }
 }
