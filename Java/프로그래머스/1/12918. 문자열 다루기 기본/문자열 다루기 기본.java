a2c33856dbcd30f28e2ad5b54174697aaf215434
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-05 19:04:02 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 73.5 MB -BaekjoonHub

--- Java/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ Java/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,5 +1,14 @@
 class Solution {
     public boolean solution(String s) {
-        return (s.length() == 4 || s.length() == 6) && s.replaceAll("[a-zA-Z]", "").length() == s.length();
+        if(s.length() == 4 || s.length() == 6) {
+            try {
+                int num = Integer.parseInt(s);
+                return true;
+            } catch (NumberFormatException e){
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 }
