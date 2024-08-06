ab1d6491ae0d3f310b386e0569c83214bd18d3a9
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-06 18:52:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.16 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                sb.append(" ");
+                index = 0;
+            } else {
+                if(index % 2 == 0) sb.append(Character.toUpperCase(s.charAt(i)));
+                else sb.append(Character.toLowerCase(s.charAt(i)));
+                index++;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
