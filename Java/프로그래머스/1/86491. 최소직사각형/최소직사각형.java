96acaeeaeefe97e98fc4a2e8684d19faf63bfde5
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-08 09:04:11 +0900

    [level 1] Title: 최소직사각형, Time: 1.83 ms, Memory: 87 MB -BaekjoonHub

--- Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ Java/프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int[][] sizes) {
         int w = 0;
         int h = 0;
-        for(int i = 0; i < sizes.length; i++) {
-            w = Math.min(sizes[i][0], sizes[i][1]) > w ? Math.min(sizes[i][0], sizes[i][1]) : w;
-            h = Math.max(sizes[i][0], sizes[i][1]) > h ? Math.max(sizes[i][0], sizes[i][1]) : h;
+        for(int[] size : sizes) {
+            w = Math.min(size[0], size[1]) > w ? Math.min(size[0], size[1]) : w;
+            h = Math.max(size[0], size[1]) > h ? Math.max(size[0], size[1]) : h;
         }
         return w * h;
     }
