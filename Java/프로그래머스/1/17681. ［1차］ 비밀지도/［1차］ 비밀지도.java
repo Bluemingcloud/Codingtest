25539f298b6ea0065d4bde8b9cc59b280f85a9ed
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-12 19:54:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 18.15 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for(int i = 0; i < n; i++) {
+        	String str = Integer.toBinaryString(arr1[i] | arr2[i]);
+        	answer[i] = String.format("%" + n + "s", str).replace("1", "#").replace("0", " ");
+        }
+        
+        return answer;
+    }
+}
