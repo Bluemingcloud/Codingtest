cae8ee507143f97d66cfd3ea997078bfa50a0090
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-13 19:38:44 +0900

    [level 1] Title: 폰켓몬, Time: 5.99 ms, Memory: 76.9 MB -BaekjoonHub

--- Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ Java/프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,8 +1,10 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        
-        
-        return Math.min((int)Arrays.stream(nums).distinct().count(), nums.length / 2);    
+    	Set<Integer> set = new HashSet<Integer>();
+    	for(int n : nums) {
+    		set.add(n);
+    	}
+    	return Math.min(set.size(), nums.length / 2);
     }
 }
