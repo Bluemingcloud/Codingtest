69ccbcb013335307bef964c07785b6517bb83eb0
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-13 18:36:37 +0900

    [level 1] Title: 콜라 문제, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

--- Java/프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ Java/프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,6 +1,6 @@
 class Solution {
     public static int solution(int a, int b, int n) {
-        if(n < a || n < 2) return 0;
-        return solution(a, b, n % a + b * (n / a)) + b * (n / a);
+        
+        return (n - b) / (a - b) * b; 
     }
 }
