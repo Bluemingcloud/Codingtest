549cb187c01323be6075eab6d1bbeb3f04f703b9
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-14 19:40:30 +0900

    [level 1] Title: 기사단원의 무기, Time: 12.40 ms, Memory: 80.5 MB -BaekjoonHub

--- Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,25 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int number, int limit, int power) {
-    	
-    	// 기사단은 1번부터 number 까지 번호가 지정
-    	// 약수의 개수 만큼 공격력에 맞춰서 무기 구매
-    	// 공격력(약수의 개수)이 limit 을 넘으면 power 로 무기 제한
-    	// 공격력 당 1kg 철 소비시 철의 총 무게 계산
         int[] arr = new int[number + 1];
-        arr[0] = 0;
-        arr[1] = 1;
-        int answer = arr[1];
         
-        for(int i = 2; i < arr.length; i++) {
-        	for(int j = 1; j <= Math.sqrt(i); j++) {
-        		if(i % j == 0) {
-        			arr[i] += 2;
-        		}
+        for(int i = 1; i <= number; i++) {
+        	for(int j = 1; j <= number / i; j++) {
+        		arr[i * j]++;
         	}
-            if((int)Math.sqrt(i) == Math.sqrt(i)) arr[i]--;
-        	if(arr[i] > limit) arr[i] = power;
-        	answer += arr[i];
+        }
+        
+        int answer = 0;
+        for(int a : arr) {
+        	answer += a > limit ? power : a;
         }
         
         return answer;
