9d98d31f42049403688331912cec8da1f0d5b515
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-14 19:31:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 151.80 ms, Memory: 83.5 MB -BaekjoonHub

--- Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ Java/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -7,13 +7,12 @@ class Solution {
     	// 공격력(약수의 개수)이 limit 을 넘으면 power 로 무기 제한
     	// 공격력 당 1kg 철 소비시 철의 총 무게 계산
         int[] arr = new int[number + 1];
-        Arrays.fill(arr, 2);
         arr[0] = 0;
         arr[1] = 1;
         int answer = arr[1];
         
         for(int i = 2; i < arr.length; i++) {
-        	for(int j = 2; j <= Math.sqrt(i); j++) {
+        	for(int j = 1; j <= Math.sqrt(i); j++) {
         		if(i % j == 0) {
         			arr[i] += 2;
         		}
