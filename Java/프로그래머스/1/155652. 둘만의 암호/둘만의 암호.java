f23cc39b71deac2e5f58591feb40ac3f907a6fb9
Bluemingcloud <bluemingcloud0911@gmail.com>
2024-08-20 21:36:42 +0900

    [level 1] Title: 둘만의 암호, Time: 1.75 ms, Memory: 75.9 MB -BaekjoonHub

--- Java/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ Java/프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -2,16 +2,19 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         StringBuilder answer = new StringBuilder();
-        for(int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            for(int j = 0; j < index; j++) {
-                c = c == 'z' ? 'a' : (char)(c + 1);
-                while(skip.contains(String.valueOf(c))) {
-                    c = c == 'z' ? 'a' : (char)(c + 1);
+        for(char c : s.toCharArray()) {
+            char temp = c;
+            int idx = 0;
+            while(idx < index) {
+                
+                temp = temp == 'z' ? 'a' : (char)(temp + 1);
+                if(!skip.contains(String.valueOf(temp))) {
+                    idx++;
                 }
+                
             }
             
-            answer.append(String.valueOf(c));
+            answer.append(String.valueOf(temp));
         }
 
         return answer.toString();
